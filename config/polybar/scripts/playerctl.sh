02ea74160780f5447fe00505c540ad7a0ecaffd0
Simon <simon@burdall.com>
2024-11-17 13:03:03 +0000

    Minor config fix.

---
+++ config/polybar/scripts/playerctl.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+VLC_ICON="󰕼"
+BROWSER_ICON="󰖟" 
+SPOTIFY_ICON="󰓇"
+MUSIC_ICON="󰝚"
+PODCAST_ICON="󰦔"
+PAUSE_ICON="󰏤"
+KON_ICON=""
+
+status=$(playerctl status 2> /dev/null)
+
+if [ "$status" == "Playing" ]; then
+    PLAYER=$(playerctl metadata --format '{{playerName}}')
+    ARTIST=$(playerctl metadata artist)
+    TITLE=$(playerctl metadata title)
+    case "$PLAYER" in
+       "firefox")
+            ICON=$BROWSER_ICON
+            ;;
+        "chromium")
+            ICON=$PODCAST_ICON
+            ;;
+        "vlc")
+            ICON=$VLC_ICON
+            ;;
+        "spotify")
+            ICON=$SPOTIFY_ICON
+            ;;
+        *)
+            ICON=$MUSIC_ICON
+            ;;
+    esac
+
+    OUTPUT="$ARTIST - $TITLE"
+    echo "$ICON $OUTPUT"
+elif [ "$status" == "Paused" ]; then
+    echo "$PAUSE_ICON Paused"
+else
+    echo "$KON_ICON \"Fun things are fun.\" - Yui"
+fi
+
---
+++ scripts/build.ksh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+## Nixos Configuration
+# Copy config into local and rebuild.
+sudo cp ../nixos/configuration.nix /etc/nixos/
+sudo nixos-rebuild switch
+
+# Copy all config directories in local.
+for dir in */; do
+    cp -r "../config/$dir" ~/.config/
+done
+
+# Copy alias to local.
+cp ../bashrc ~/.bashrc
+source ~/.bashrc
+
+# Build the file structure.
+./create_directories.sh
+
+# Setup AstroNvim.
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+nvim

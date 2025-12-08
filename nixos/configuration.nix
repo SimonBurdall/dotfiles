e06a0304b0306b45ea451839ec1d61e9d61de577
Simon <simon@burdall.com>
2025-12-08 17:06:04 +0000

    20251208-367992-rits

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -192,6 +192,8 @@
     openFirewall = true;
   };
 
+  services.tailscale.enable = true;
+
   services.udev.extraRules = ''
     # Meta Quest USB detection (for ADB/sideloading)
     SUBSYSTEM=="usb", ATTR{idVendor}=="2833", MODE="0660", GROUP="plugdev", TAG+="uaccess"

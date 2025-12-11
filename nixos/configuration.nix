bd0b514ff013476ee3463a01adfec3de4e31ecea
Simon <simon@burdall.com>
2025-12-11 19:14:36 +0000

    20251211-436466-rits

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -234,9 +234,15 @@
       ];
   };
 
-  programs.alvr = {
+  #programs.alvr = {
+  #  enable = true;
+  #  openFirewall = true;
+  #};
+
+  services.wivrn = {
     enable = true;
     openFirewall = true;
+    defaultRuntime = true;
   };
 
   ## Package Management ----

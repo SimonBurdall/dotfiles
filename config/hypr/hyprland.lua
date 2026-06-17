fbdecd9267fc29c88056dd15f98870930ca1b5ac
Simon <simon@burdall.com>
2026-06-17 09:07:43 +0100

    20260617-541455-rits

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -119,11 +119,6 @@ hl.window_rule({
 	suppress_event = "maximize",
 })
 
-hl.window_rule({
-	match = { class = "kitty" },
-	opacity = "0.85 0.85",
-})
-
 ---- KEYBINDS    ---
 local mod = "SUPER"
 local moon = "CTRL + SHIFT + ALT + SUPER"
